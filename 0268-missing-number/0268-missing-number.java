32ed0ae69bb5a2d9d4cbc66a7ec3321cba34cf9a
VijayBhandari2002 <149879227+VijayBhandari2002@users.noreply.github.com>
2025-07-16 02:37:52 +0530

    Time: 0 ms (100%), Space: 45.5 MB (43.38%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,11 +1,13 @@
 class Solution {
     public int missingNumber(int[] nums) {
         int n = nums.length;
-        int expectedSum = n*(n+1)/2;
-        int actualSum =0;
-        for(int i=0;i<nums.length;i++){
-            actualSum+=nums[i];
+        int realSum = (n*(n+1))/2;
+        int sum =0;
+
+        for(int i = 0; i<nums.length;i++){
+            sum+= nums[i];
         }
-        return expectedSum-actualSum;
+        return realSum - sum;
+
     }
 }
